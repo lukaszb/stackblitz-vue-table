12e33d889450cae9a6a7ee01bf6da401b3d55dee
lukaszb <lukaszbalcerzak@gmail.com>
2022-05-02 10:50:03 +0200

    try jsx

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,14 @@
-<script setup lang="tsx">
-const props = defineProps({
-  data: [Array],
-});
+<script lang="tsx">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    data: Array
+  },
+  setup(props) {
+    return () => (
+      <div>Hello world</div>
+    )
+  }
+})
 </script>
